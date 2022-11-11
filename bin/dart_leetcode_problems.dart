60912bbb0c4e86688fcc6a008e9cfaddf0a98f13
Gwhyyy <ffikhi.aanas@gmail.com>
2022-11-11 23:50:23 +0100

    [ Add ] added starter files

---
+++ bin/dart_leetcode_problems.dart
@@ -0,0 +1,5 @@
+import 'package:dart_leetcode_problems/dart_leetcode_problems.dart' as dart_leetcode_problems;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_leetcode_problems.calculate()}!');
+}
---
+++ test/dart_leetcode_problems_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_leetcode_problems/dart_leetcode_problems.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
